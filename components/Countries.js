043a53ce4121b2b75571a24b8f848e5acc3f0a65
Abiona Michael <amabiona21@gmail.com>
2021-02-02 13:37:52 +0100

    sorting countries by population

--- components/Countries.js
+++ components/Countries.js
@@ -1,18 +1,38 @@
+import { KeyboardArrowDownRounded } from '@material-ui/icons';
 import styles from '../styles/Countries.module.css';
 
+const orderBy = (countries, direction) => {
+  if(direction === 'asc') {
+    return [...countries].sort((a, b) => (a.popluation > b.population ? 1 : -1));
+  }
+
+  if(direction === 'desc'){
+    return [...countries].sort((a, b) => (a.population > b.population ? -1 : 1));
+  }
+
+  return countries;
+}
+
 const CountriesTable = ({ countries }) => {
+  const orderedCountries = orderBy(countries, 'desc')
   return (
     <div>
       <div className={styles.heading}>
         <button className={styles.heading_name}>
           <div>Name</div>
+          <div className={styles.heading_arrow}>
+            <KeyboardArrowDownRounded color="inherit" />
+          </div>
         </button>
         <button className={styles.heading_population}>
           <div>Population</div>
+          <div className={styles.heading_arrow}>
+            <KeyboardArrowDownRounded color="inherit" />
+          </div>
         </button>
       </div>
 
-      {countries.map((country) => (
+      {orderedCountries.map((country) => (
         <div className={styles.row}>
           <div className={styles.name}>{country.name}</div>
 
